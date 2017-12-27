c3970df1edb41c18abf5f1b3b7fc3ef214f16b67
Nigel <hypej.10@gmail.com>
2017-12-27 18:58:30 +0200

    stripe creds

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,5 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
-}
+  :publishable_key => 'pk_test_mMy5ioKjA3c0s1hvxmmPxoSK',
+  :secret_key      => 'sk_test_cWcdjtk7BseAOhbw1aGCdvx3'}
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
