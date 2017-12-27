da611bd01e1d0cd4b2d3a72d735685783b906408
Nigel <hypej.10@gmail.com>
2017-12-27 23:59:58 +0200

    add show view

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,6 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update, :destroy]
-
+  layout "front_end"
   # GET /rooms
   # GET /rooms.json
   def index
