a313259f6ccce507c5a8eeb3773e382f190b69a5
john  kyony <jkyony@gmail.com>
2017-12-28 11:20:36 +0000

    the fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :charges , only: [:new , :create]
+  get 'thanks' , to: 'charges#thanks' , as: 'thanks'
   resources :rooms
   devise_for :users
   resources :users
