43efa01463fdfe96c24851359d8997c4f11d5658
Nigel <hypej.10@gmail.com>
2017-12-29 01:31:41 +0200

    fix booking flow

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -1,15 +1,22 @@
 class BookingController < ApplicationController
   before_action :authenticate_user!
+  layout "front_end", only: [:new]
   
   def index
     @user_bookings = current_user.bookings
   end
   def new
+    @room = Room.find(params[:room_id])
   end
 
   def create
-    room = params[:room_id]
-    current_user.book! room
+    @room = Room.find(params[:room_id])
+    puts @room.number
+    current_user.book! @room
     redirect_to booking_index_path
   end
+end
+
+def booking_params
+  params.require(:room).permit(:room_id)
 end
